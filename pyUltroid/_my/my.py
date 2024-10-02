aab0610f400a7873836b5cc48564f009f2e9f8d0
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-10-03 03:36:13 +0530

    Update my.py

--- pyUltroid/_my/my.py
+++ pyUltroid/_my/my.py
@@ -65,6 +65,6 @@ async def photo(e):
 async def cpp(e):
   c = await e.eor(f"`processing`")
   a,b = await bash(f"curl -L https://gist.githubusercontent.com/rooted-cyber/1bd2b7d3eb4d66ab06ab5e83098395e3/raw/cpp | bash")
-  await c.edit(f"{a}\n\n")
+  await msg(e,f"{a}\n\n")
   
 
