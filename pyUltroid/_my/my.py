f5f5e7f28a528fea95dadbb0b5cb05c4c47e3952
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-10-02 21:31:13 +0530

    Update my.py

--- pyUltroid/_my/my.py
+++ pyUltroid/_my/my.py
@@ -16,7 +16,7 @@ async def leave_group(group_username):
 async def msg(e,ab):
     await e.client.send_message(e.chat_id,f"{ab}")
 
-async def dl(e):
+async def dla(e):
     mkdir("plugins-file")
     cd("plugins-file")
     reply = await e.get_reply_message()
@@ -54,6 +54,7 @@ async def photo(e):
       path = await e.client.download_profile_photo("me")
       await e.reply(f"**My profile pic**",file=path)
 
+
 async def await dl():
-  await dl(e)
+    await dla(e)
 
