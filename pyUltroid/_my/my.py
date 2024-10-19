32516eca50c1be57e510fdc4f300285a96067a53
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-10-20 02:00:06 +0530

    Update my.py

--- pyUltroid/_my/my.py
+++ pyUltroid/_my/my.py
@@ -2,6 +2,7 @@ from telethon import TelegramClient
 from telethon.tl.functions.channels import LeaveChannelRequest
 from os import listdir as ls, remove as rm, mkdir, chdir as cd, rmdir, removedirs as rmd
 from ..fns.helper import bash
+from ..fns.tools import get_paste
 from time import strftime as aj
 from ..startup.utils import load_addons
 #import akenoai as ak
@@ -65,3 +66,50 @@ async def join(pp):
     from telethon.tl.functions.channels import JoinChannelRequest
     i = f"pp"
     await bot(JoinChannelRequest(f"{i}"))
+
+
+async def pst(event):
+    try:
+        input_str = event.text.split(maxsplit=1)[1]
+    except IndexError:
+        input_str = None
+    xx = await event.eor("` 《 Pasting... 》 `")
+    downloaded_file_name = None
+    if input_str:
+        message = input_str
+    elif event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        if previous_message.media:
+            downloaded_file_name = await event.client.download_media(
+                previous_message,
+                "./resources/downloads",
+            )
+            with open(downloaded_file_name, "r") as fd:
+                message = fd.read()
+            os.remove(downloaded_file_name)
+        else:
+            message = previous_message.message
+    else:
+        message = None
+    if not message:
+        return await xx.eor(
+            "`Reply to a Message/Document or Give me Some Text !`", time=5
+        )
+    done, key = await get_paste(message)
+    if not done:
+        return await xx.eor(key)
+    link = f"https://spaceb.in/{key}"
+    raw = f"https://spaceb.in/api/v1/documents/{key}/raw"
+    reply_text = (
+        f"• **Pasted to SpaceBin :** [Space]({link})\n• **Raw Url :** : [Raw]({raw})"
+    )
+    try:
+        if event.client._bot:
+            return await xx.eor(reply_text)
+        ok = await event.client.inline_query(asst.me.username, f"pasta-{key}")
+        await ok[0].click(event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True)
+        await xx.delete()
+    except BaseException as e:
+        LOGS.exception(e)
+        await xx.edit(reply_text)
+
