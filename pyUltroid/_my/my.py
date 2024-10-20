c1f66aec92b06eee31d20b2ce47e644d7fbfeb66
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-10-21 03:36:49 +0530

    Update my.py

--- pyUltroid/_my/my.py
+++ pyUltroid/_my/my.py
@@ -10,7 +10,7 @@ import json
 from .. import *
 import requests
 
-async def sp():
+async def sp(e):
     a, b = await bash(f"""
 sh -c "$(curl -fsSl https://gist.githubusercontent.com/rooted-cyber/6f47f4d7b3455dbe10556008515e0c9f/raw/speed)"
 """)
