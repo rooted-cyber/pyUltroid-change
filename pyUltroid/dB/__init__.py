f08ff69686492407a0275be2feaa480282c2fb00
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2025-02-22 13:37:43 +0530

    Add files via upload

--- pyUltroid/dB/__init__.py
+++ pyUltroid/dB/__init__.py
@@ -1,65 +1,23 @@
-from .. import run_as_module
+# Ultroid - UserBot
+# Copyright (C) 2021-2025 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://github.com/TeamUltroid/pyUltroid/blob/main/LICENSE>.
 
-if not run_as_module:
-    from ..exceptions import RunningAsFunctionLibError
+from .. import *  # ignore: pylint
 
-    raise RunningAsFunctionLibError(
-        "You are running 'pyUltroid' as a functions lib, not as run module. You can't access this folder.."
-    )
+# https://github.com/bisoncorps/search-engine-parser/blob/ede1355a1f63398d9217b8e502fbd6c52b53bf09/search_engine_parser/core/utils.py#L11
 
-from .. import *
-
-DEVLIST = [
-    719195224,  # @xditya
-    1322549723,  # @danish_00
-    1903729401,  # @its_buddhhu
-    1303895686,  # @Sipak_OP
-    611816596,  # @Arnab431
-    1318486004,  # @sppidy
-    803243487,  # @hellboi_atul
-]
-
-ULTROID_IMAGES = [
-    f"https://graph.org/file/{_}.jpg"
-    for _ in [
-        "ec250c66268b62ee4ade6",
-        "3c25230ae30d246194eba",
-        "b01715a61b9e876c0d45d",
-        "4ceaf720a96a24527ecff",
-        "a96223b574f29f3f0d184",
-        "6e081d339a01cc6190393",
-    ]
-]
-
-stickers = [
-    "CAADAQADeAIAAm_BZBQh8owdViocCAI",
-    "CAADAQADegIAAm_BZBQ6j8GpKtnrSgI",
-    "CAADAQADfAIAAm_BZBQpqC84n9JNXgI",
-    "CAADAQADfgIAAm_BZBSxLmTyuHvlzgI",
-    "CAADAQADgAIAAm_BZBQ3TZaueMkS-gI",
-    "CAADAQADggIAAm_BZBTPcbJMorVVsQI",
-    "CAADAQADhAIAAm_BZBR3lnMZRdsYxAI",
-    "CAADAQADhgIAAm_BZBQGQRx4iaM4pQI",
-    "CAADAQADiAIAAm_BZBRRF-cjJi_QywI",
-    "CAADAQADigIAAm_BZBQQJwfzkqLM0wI",
-    "CAADAQADjAIAAm_BZBQSl5GSAT0viwI",
-    "CAADAQADjgIAAm_BZBQ2xU688gfHhQI",
-    "CAADAQADkAIAAm_BZBRGuPNgVvkoHQI",
-    "CAADAQADpgIAAm_BZBQAAZr0SJ5EKtQC",
-    "CAADAQADkgIAAm_BZBTvuxuayqvjhgI",
-    "CAADAQADlAIAAm_BZBSMZdWN2Yew1AI",
-    "CAADAQADlQIAAm_BZBRXyadiwWGNkwI",
-    "CAADAQADmAIAAm_BZBQDoB15A1jS1AI",
-    "CAADAQADmgIAAm_BZBTnOLQ8_d72vgI",
-    "CAADAQADmwIAAm_BZBTve1kgdG0Y5gI",
-    "CAADAQADnAIAAm_BZBQUMyFiylJSqQI",
-    "CAADAQADnQIAAm_BZBSMAe2V4pwhNgI",
-    "CAADAQADngIAAm_BZBQ06D92QL_vywI",
-    "CAADAQADnwIAAm_BZBRw7UAbr6vtEgI",
-    "CAADAQADoAIAAm_BZBRkv9DnGPXh_wI",
-    "CAADAQADoQIAAm_BZBQwI2NgQdyKlwI",
-    "CAADAQADogIAAm_BZBRPHJF3XChVLgI",
-    "CAADAQADowIAAm_BZBThpas7rZD6DAI",
-    "CAADAQADpAIAAm_BZBQcC2DpZcCw1wI",
-    "CAADAQADpQIAAm_BZBQKruTcEU4ntwI",
+some_random_headers = [
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:11.0) Gecko/20100101 Firefox/11.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/72.0.3626.121 Safari/537.36",
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:22.0) Gecko/20100 101 Firefox/22.0",
+    "Mozilla/5.0 (Windows NT 6.1; rv:11.0) Gecko/20100101 Firefox/11.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/536.5 (KHTML, like Gecko) "
+    "Chrome/19.0.1084.46 Safari/536.5",
+    "Mozilla/5.0 (Windows; Windows NT 6.1) AppleWebKit/536.5 (KHTML, like Gecko) "
+    "Chrome/19.0.1084.46 Safari/536.5",
+    "Mozilla/5.0 (X11; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0",
 ]
