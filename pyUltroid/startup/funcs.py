a42d210e1b5c50f963e2f886ae87957cbf727308
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2025-03-16 22:15:31 +0530

    Update funcs.py

--- pyUltroid/startup/funcs.py
+++ pyUltroid/startup/funcs.py
@@ -520,7 +520,7 @@ async def WasItRestart(udb):
         if "rm" in ls("/data/data/com.termux/files/usr/bin"):
             s("ra")
         else:
-            await asst.dend_message("**Already**")
+            await asst.send_message("**Already**")
         data = key.split("_")
         who = asst if data[0] == "bot" else ultroid_bot
         await who.edit_message(
